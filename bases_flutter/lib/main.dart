1ffc85277fa7188b3eaee56b358751947cd0878a
popehiflo <poolpetterhijuel@hotmail.com>
2021-08-18 11:10:09 -0500

    Bases en Flutter

---
+++ bases_flutter/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:bases_flutter/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bases Flutter',
+      home: HomeScreen(),
+    );
+  }
+}

45b14fc0c35e870f1811453c6b8186715da532d0
Pool Petter Hijuela Florian <poolpetterhijuel@hotmail.com>
2022-01-02 11:01:40 -0500

    acercamiento a flutter

--- hello_flutter/lib/main.dart
+++ hello_flutter/lib/main.dart
@@ -96,12 +96,13 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Has pulsado el boton todas estas veces:',
             ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            ElevatedButton(onPressed: _incrementCounter, child:  const Text('contador'))
           ],
         ),
       ),
